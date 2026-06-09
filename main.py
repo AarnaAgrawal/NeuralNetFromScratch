f40597b06b8a06278555447be5df27fc72302d5f
aarna ✨ <82003715+AarnaAgrawal@users.noreply.github.com>
2026-06-08 17:54:23 -0700

    adding biasssssss

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 x_vals=[1,2,3,5,8,10]
-y_vals=[2,4,6,10,16,20]
+y_vals=[3,5,7,11,17,21]
 def train(x_values, y_values):
     w=1.0
+    b=0.0
     for epoch in range(1000):
         for xval,yval in zip(x_values, y_values):
-            prediction=w*xval
+            prediction=w*xval+b
             loss=(prediction-yval)**2
-            gradient=2*(w*xval-yval)*xval
-            w=w-0.01*gradient
+            gradient_w=2*(w*xval+b-yval)*xval
+            gradient_b=2*(w*xval+b-yval)
+            w=w-0.01*gradient_w
+            b=b-0.01*gradient_b
             print(w)
-    return w
-print(train(x_vals, y_vals)*16)
+            print(b)
+    return w, b
+w, b=train(x_vals,y_vals)
+print(w)
+print(b)
+print(18*w+b)
